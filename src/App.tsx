ea6e87510b3131c5022395bc335f39ab7a03989f
Tamires Patrocinio <tamirespatrocinio.ti@gmail.com>
2022-08-13 15:57:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World!</h1>
+    </ThemeProvider>
+  );
+}
